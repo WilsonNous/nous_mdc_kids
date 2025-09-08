404ea3e9422cbf207326b5baad8dfc03628ee2ea
WilsonNous <107373913+WilsonNous@users.noreply.github.com>
2025-09-08 15:55:36 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,11 @@ from mysql.connector import Error
 def get_db_connection():
     try:
         connection = mysql.connector.connect(
-            host='108.167.132.58',
-            database='noust785_crm_mdc_canasvieiras',
-            user='noust785_admin',
-            password='M@st3rk3y')
+            host=os.getenv('MYSQL_HOST'),
+            database=os.getenv('MYSQL_DB'),
+            user=os.getenv('MYSQL_USER'),
+            password=os.getenv('MYSQL_PASSWORD')
+        )
         return connection
     except Error as e:
         print(f"Erro ao conectar ao MySQL: {e}")
